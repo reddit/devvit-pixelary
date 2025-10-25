0a0782bed024247bda2d6666ad870f12e430a2e5
Knut Synstad <knut.synstad@reddit.com>
2025-10-24 22:04:32 -0700

    Update Shimmer.tsx

--- src/client/components/Shimmer.tsx
+++ src/client/components/Shimmer.tsx
@@ -1,10 +1,10 @@
 export function Shimmer() {
   return (
     <div
-      className="fixed inset-0 pointer-events-none z-50 animate-diagonal-shimmer"
+      className="fixed inset-0 pointer-events-none animate-diagonal-shimmer"
       style={{
         background:
-          'linear-gradient(-45deg, transparent 40%, rgba(255, 255, 255, 0.1) 50%, transparent 60%)',
+          'linear-gradient(-45deg, transparent 40%, rgba(255, 255, 255, 0.2) 50%, transparent 60%)',
         backgroundSize: '300%',
         backgroundPositionX: '100%',
       }}
